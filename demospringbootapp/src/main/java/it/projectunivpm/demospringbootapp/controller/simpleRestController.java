bb8685ee83b7a8f12dc8facc94004dbf5376b5e7
MarcoSpina01 <94124443+MarcoSpina01@users.noreply.github.com>
2021-12-15 17:14:54 +0100

    Update simpleRestController.java

--- demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/controller/simpleRestController.java
+++ demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/controller/simpleRestController.java
@@ -1,4 +1,6 @@
 package it.projectunivpm.demospringbootapp.controller;
+import it.projectunivpm.demospringbootapp.model.*;
+import it.projectunivpm.demospringbootapp.service.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,29 +15,38 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 public class simpleRestController {
-	
+
+
+	ConnectionImpl Connectionimpl = new ConnectionImpl();
+
+	@RequestMapping(value = "/page_consumptions") 
+	public ResponseEntity<Object> getInsightImpl() {
+		Connectionimpl.saveData();
+		return new ResponseEntity<>(InsightImpl.getInsightImpl(), HttpStatus.OK);
+	}
+
 	/* 
 	 * Il numero di volte in cui le persone hanno cliccato su uno dei tuoi contenuti
 	 */
-	
+
 	/*
 	 * @GetMapping("/page_consumptions")
 	 */
 
 	/* 
 	 * Il numero di volte in cui un contenuto della tua Pagina o relativo alla
-	 * tua Pagina è apparso nello schermo di una persona. Sono inclusi i post, 
+	 * tua Pagina e' apparso nello schermo di una persona. Sono inclusi i post, 
 	 * le storie, le inserzioni e altri contenuti o informazioni presenti sulla tua Pagina.
 	 */
-	
+
 	/*
 	 * @GetMapping("/page_impressions")
 	 */
-	
+
 	/* 
 	 * Il numero di nuove persone a cui piace la tua Pagina.
 	 */
-	
+
 	/*
 	 * @GetMapping("/page_fan_adds_unique")
 	 */	
