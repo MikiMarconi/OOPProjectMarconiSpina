3ed8abf00f38f6ae84faef7cce477ffa875b30a9
MarcoSpina01 <94124443+MarcoSpina01@users.noreply.github.com>
2021-12-15 17:10:35 +0100

    Update ConnectionImpl.java
    Sviluppo del parsing in maniera corretta

--- demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/service/ConnectionImpl.java
+++ demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/service/ConnectionImpl.java
@@ -16,13 +16,14 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class ConnectionImpl implements ConnectionService{
-	
+
+	//InsightImpl insightImpl = new InsightImpl();
 	/*
 	 * Stabiliamo una connessione con metodo GET per ottenere i dati dalle api di Facebook
 	 */
 
 	public void saveData() {
-		String accessToken = "EAAPR8XUrBo8BAHCKLhOZASIfTsLpz6Qo89B1Nvs1U9puKlwuQGimC0YanfPjhmiGy0yidZBsmCmDcY0cVvy5cA9IcLphthqzaLfR6i5psKoGgDBZAYwIpsRrxHfEeJqKyqVtYXelkUiElvzYF5mT0H9RiuCTDSXWFZCXwdHYrWB7LlLE1UpvWKK1JZC0zo10q0ZA3uac95DQZDZD";			
+		String accessToken = "EAAPR8XUrBo8BABdmr1wC1o8qqNhSJNGZBxyJc1ZBWZA2eUqIxgZAPWZBKV1yTu0XE6I9gSa5SWHA0ZCkNeTYj3Ry8IaLxBwyekmqeSUVNzT46kfjjwcZBoHNvl9umhqgoWw8mm8ouG04xC6MlK6XdokRDZBjZClVQCH0EQsLjsbUyZBmZCNT9NMfvZCPbMQeOAQJOMun2skMvGXYMRCvusMRfPiM";
 		try {
 			URL url = new URL("https://graph.facebook.com/v12.0/121258985205219/insights?metric=page_impressions%2Cpage_consumptions%2Cpage_fan_adds_unique&access_token=" + accessToken);		 
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -33,40 +34,40 @@ public class ConnectionImpl implements ConnectionService{
 			/*
 			 * Controlliamo che la connessione sia andata a buon fine
 			 */
-			
+
 			int responseCode = conn.getResponseCode();
 
 			/*
-			 * Se non è 200 , inviamo un'eccezione
+			 * Se non Ã¨ 200 , inviamo un'eccezione
 			 */
-			
+
 			if (responseCode != 200) {
 				throw new RuntimeException("HttpResponseCode:" + responseCode);
-			} else {
+			} 
+			else {
+
+				/*
+				 * Apriamo un flusso di input dal nostro oggetto URL e un flusso di output su file locale
+				 */
+
+				Scanner input = new Scanner(url.openStream());
+				BufferedWriter writer =
+						new BufferedWriter (new FileWriter ("salvadati.txt"));
+
+				/*
+				 * Copiamo tutti i dati provenienti dalle API nel file
+				 */
+
+				while (input.hasNext()) {
+
+					writer.write(input.nextLine());
+				}
+				writer.close();
+				input.close();
+
+				conn.disconnect();
 
 			}
-			
-			/*
-			 * Apriamo un flusso di input dal nostro oggetto URL e un flusso di output su file locale
-			 */
-			
-			Scanner input = new Scanner(url.openStream());
-			BufferedWriter writer =
-					new BufferedWriter (new FileWriter ("salvadati.txt"));
-			
-			/*
-			 * Copiamo tutti i dati provenienti dalle API nel file
-			 */
-			
-			while (input.hasNext()) {
-
-				writer.write(input.nextLine());
-			}
-			writer.close();
-			input.close();
-
-			conn.disconnect();
-
 		}  catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -106,25 +107,24 @@ public class ConnectionImpl implements ConnectionService{
 			
 				obj = parser.parse(reader);
 
-				InsightImpl Insightimpl = new InsightImpl();
+				
 				JSONObject empjsonobj = (JSONObject) obj ;
 				JSONArray array = (JSONArray)empjsonobj.get("data");
-				long[] temp = {0,0};
-				for (int i=0;i<9;i++) {
+				for (int i=0;i<array.size();i++) {
 					JSONObject obj2 = (JSONObject) array.get(i);
 					Insight in = new Insight();
 					in.setDescription((String) obj2.get("description"));
 					in.setTitle((String) obj2.get("title"));
 					in.setPeriod((String) obj2.get("period"));
 					JSONArray array2 = (JSONArray) obj2.get("values");
-					for (int j=0;j<2;j++) {
-						JSONObject obj3 = (JSONObject) array2.get(j);
-						temp[j] = (Long)obj3.get("value");
 
-					}
-					in.setValues(temp);
-					Insightimpl.getInsightImpl().add(in);
+					JSONObject obj3 = (JSONObject) array2.get(0);
+					in.setValue1((long) obj3.get("value"));
+					JSONObject obj4 = (JSONObject) array2.get(1);
+					in.setValue2((long) obj4.get("value"));
+					InsightImpl.getInsightImpl().add(in);
 				}
+				
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
