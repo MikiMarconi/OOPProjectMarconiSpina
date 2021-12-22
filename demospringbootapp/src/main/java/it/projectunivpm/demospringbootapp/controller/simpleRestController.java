bd45e02eb2f02110b9fd51b4429fe59d8b102f8f
MarcoSpina01 <94124443+MarcoSpina01@users.noreply.github.com>
2021-12-22 18:39:02 +0100

    Update simpleRestController.java
    Gestione eccezioni nel controller

--- demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/controller/simpleRestController.java
+++ demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/controller/simpleRestController.java
@@ -1,61 +1,80 @@
 package it.projectunivpm.demospringbootapp.controller;
-import it.projectunivpm.demospringbootapp.filter.Filter;
 import it.projectunivpm.demospringbootapp.model.*;
 import it.projectunivpm.demospringbootapp.service.*;
-import it.projectunivpm.demospringbootapp.service.ParsingPageImpressions.ParsingPageImpressionDayImpl;
-import it.projectunivpm.demospringbootapp.service.ParsingPageImpressions.ParsingPageImpressionWeekImpl;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import org.json.simple.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import it.projectunivpm.demospringbootapp.exceptions.MetricNotFoundException;
+import it.projectunivpm.demospringbootapp.exceptions.MetricOrPeriodNotFoundException;
+import it.projectunivpm.demospringbootapp.exceptions.StatisticNotFoundException;
+import it.projectunivpm.demospringbootapp.filter.Filter;
+
+
 
 @RestController
 public class simpleRestController {
 
 	ConnectionTotalInsightImpl Connectionimpl = new ConnectionTotalInsightImpl();
 
-	@RequestMapping(value = "/total_insight") 
+	@RequestMapping(value = "/insight") 
 	public ResponseEntity<Object> getTotalInsight() {
 		Connectionimpl.saveData();
 		return new ResponseEntity<>(InsightImpl.getInsightImpl(), HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/{metric}/{period}") 
-	public ResponseEntity<Insight> getFilteredInsight(@PathVariable String metric,@PathVariable String period) {
-		Connectionimpl.saveData();
-		return new ResponseEntity<>(Search.searchByMetricAndPeriod(metric, period), HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/{metric}") 
+	@RequestMapping(value = "/insight/{metric}") 
 	public ResponseEntity<Object> getInsightByMetric(@PathVariable String metric) {
 		Connectionimpl.saveData();
-		return new ResponseEntity<>(Search.searchByMetric(metric), HttpStatus.OK);
+		try {
+			return new ResponseEntity<>(Search.searchByMetric(metric), HttpStatus.OK);
+		} 
+		catch (MetricNotFoundException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
-	@RequestMapping(value = "/insight/{statistic}") 
+	@RequestMapping(value = "/insight/stats/{statistic}") 
 	public ResponseEntity<Object> getStatistics(@PathVariable String statistic) {
 		Connectionimpl.saveData();
-		return new ResponseEntity<>(Search.searchStatsByName(statistic), HttpStatus.OK);
+		try {
+			return new ResponseEntity<>(Search.searchStatsByName(statistic), HttpStatus.OK);
+		} catch (StatisticNotFoundException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
-	@GetMapping(value = "/{metric}/{period}/{initialValue}/{finalValue}") 
-	public ResponseEntity<Object> getFilteredStatistics(@PathVariable String metric, @PathVariable String period, @PathVariable long initialValue, @PathVariable long finalValue) {
+	@RequestMapping(value = "/insight/{metric}/{period}") 
+	public ResponseEntity<Object> getFilteredInsight(@PathVariable String metric,@PathVariable String period) throws MetricOrPeriodNotFoundException {
 		Connectionimpl.saveData();
-		return new ResponseEntity<>(Filter.PageFilteredByMetricAndPeriod(Search.searchByMetricAndPeriod(metric, period), initialValue, finalValue), HttpStatus.OK);
-	
+		try {
+			return new ResponseEntity<>(Search.searchByMetricAndPeriod(metric, period), HttpStatus.OK);
+		} 
+		catch (MetricOrPeriodNotFoundException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@GetMapping(value = "/insight/{metric}/{period}/{initialValue}/{finalValue}") 
+	public ResponseEntity<Object> getFilteredStatistics(@PathVariable String metric, @PathVariable String period, @PathVariable long initialValue, @PathVariable long finalValue) throws MetricOrPeriodNotFoundException {
+		Connectionimpl.saveData();
+		try {
+			return new ResponseEntity<>(Filter.PageFilteredByMetricAndPeriod(Search.searchByMetricAndPeriod(metric, period), initialValue, finalValue), HttpStatus.OK);
+		}
+		catch (MetricOrPeriodNotFoundException e){
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
-	@GetMapping(value = "/{metric}/{initialValue}/{finalValue}") 
+	@GetMapping(value = "/insight/{metric}/{initialValue}/{finalValue}") 
 	public ResponseEntity<Object> getFilteredStatistics(@PathVariable String metric, @PathVariable long initialValue, @PathVariable long finalValue) {
 		Connectionimpl.saveData();
-		return new ResponseEntity<>(Filter.PageFilteredByMetric(Search.searchByMetric(metric), initialValue, finalValue), HttpStatus.OK);
+		try {
+			return new ResponseEntity<>(Filter.PageFilteredByMetric(Search.searchByMetric(metric), initialValue, finalValue), HttpStatus.OK);
+		} catch (MetricNotFoundException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	
 	}
 	
