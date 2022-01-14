d6e220ed2a343d8d42536311d8bdaed2d47a68f4
MarcoSpina01 <94124443+MarcoSpina01@users.noreply.github.com>
2022-01-14 14:49:42 +0100

    Update TestSearch.java

--- demospringbootapp/src/test/java/it/projectunivpm/demospringbootapp/TestSearch.java
+++ demospringbootapp/src/test/java/it/projectunivpm/demospringbootapp/TestSearch.java
@@ -2,7 +2,7 @@ package it.projectunivpm.demospringbootapp;
 
 import org.junit.jupiter.api.Test;
 
-import it.projectunivpm.demospringbootapp.exceptions.MetricOrPeriodNotFoundException;
+import it.projectunivpm.demospringbootapp.Exceptions.MetricOrPeriodNotFoundException;
 import it.projectunivpm.demospringbootapp.service.ConnectionTotalInsightImpl;
 import it.projectunivpm.demospringbootapp.service.Search;
 import junit.framework.TestCase;
