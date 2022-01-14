afc878b84cbac84f351ecac30d04706f79db0bae
MikiMarconi <94007044+MikiMarconi@users.noreply.github.com>
2022-01-14 14:03:37 +0100

    Update DemospringbootappApplication.java
    Update classe DemospringbootappApplication

--- demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/DemospringbootappApplication.java
+++ demospringbootapp/src/main/java/it/projectunivpm/demospringbootapp/DemospringbootappApplication.java
@@ -3,6 +3,11 @@ package it.projectunivpm.demospringbootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author MichelangeloMarconi
+ * @author MarcoSpina
+ */
+
 @SpringBootApplication
 public class DemospringbootappApplication {
 
